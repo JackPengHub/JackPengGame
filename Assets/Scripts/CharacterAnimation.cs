7090af828c733f1936e10dd4dce73f4aabfefa63
JackPengHub <pength@live.com>
2015-03-26 09:51:32 +0800

    update

---
+++ Assets/Scripts/CharacterAnimation.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterAnimation : Animation {
+
+	// Use this for initialization
+	void Start () {
+		GameObject characterObj = GameObject.Find ("Take 001");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
